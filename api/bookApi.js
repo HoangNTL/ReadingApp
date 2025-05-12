d58e6de1386890d964a8a8fce241dc6ac6182df9
Hoangcutedz <hoangn2422@gmail.com>
2025-05-12 16:58:39 +0700

    home screen, search screen

--- api/bookApi.js
+++ api/bookApi.js
@@ -9,3 +9,23 @@ export const getBooks = async () => {
     throw error;
   }
 };
+
+export const getTopViewedBooks = async () => {
+  try {
+    const response = await api.get('/books/top-viewed');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching top viewed books:', error);
+    throw error;
+  }
+};
+
+export const getLatestBooks = async () => {
+  try {
+    const response = await api.get('/books/latest');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching latest books:', error);
+    throw error;
+  }
+};
--- screens/Search/index.jsx
+++ screens/Search/index.jsx
@@ -1,18 +1,238 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import globalStyle from '../../assets/styles/GlobalStyle';
 import {getFontFamily} from '../../assets/fonts/helper';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+import {faEye} from '@fortawesome/free-solid-svg-icons';
+import {faList} from '@fortawesome/free-solid-svg-icons';
+import {getBooks} from '../../api/bookApi'; // Adjust the import based on your API structure
 
 const SearchScreen = ({navigation}) => {
+  const [books, setBooks] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const data = await getBooks();
+        setBooks(data);
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
   return (
     <SafeAreaView style={globalStyle.androidSafeArea}>
-      <Text
+      <View
         style={{
-          fontFamily: getFontFamily('Inter', '600'),
-          fontSize: 20,
+          flex: 1,
+          backgroundColor: '#fff',
+          paddingBottom: 56,
         }}>
-        Search
-      </Text>
+        {/* Search Bar */}
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            height: 40,
+            borderColor: '#95a5a6',
+            borderWidth: 1,
+            margin: 10,
+            paddingLeft: 10,
+            borderRadius: 24,
+            backgroundColor: '#fff',
+          }}>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            size={20}
+            color="#95a5a6"
+            style={{marginRight: 10}}
+          />
+          <TextInput
+            style={{
+              flex: 1,
+              height: '100%',
+            }}
+            placeholder="Search for books..."
+          />
+        </View>
+
+        <View
+          style={{
+            paddingHorizontal: 8,
+            marginBottom: 60,
+          }}>
+          <FlatList
+            data={books}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    width: '100%',
+                    height: 120,
+                    margin: 8,
+                  }}>
+                  <Image
+                    source={{
+                      uri: item.cover_image,
+                    }}
+                    style={{
+                      width: 80,
+                      height: '100%',
+                      backgroundColor: '#ddd',
+                      marginRight: 10,
+                    }}
+                  />
+                  {/* Book information */}
+                  <View
+                    style={{
+                      // display: 'flex',
+                      justifyContent: 'space-between',
+                    }}>
+                    {/* Title */}
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        fontWeight: 'bold',
+                        color: '#2c3e50',
+                      }}>
+                      {item.title}
+                    </Text>
+
+                    {/* Author */}
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        color: '#95a5a6',
+                      }}>
+                      {item.author}
+                    </Text>
+
+                    {/* Book description */}
+                    <View
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        gap: 10,
+                      }}>
+                      {/* Views */}
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                        }}>
+                        <FontAwesomeIcon
+                          icon={faEye}
+                          size={16}
+                          color="#95a5a6"
+                          style={{
+                            marginRight: 5,
+                          }}
+                        />
+                        <Text
+                          style={{
+                            fontSize: 12,
+                            color: '#95a5a6',
+                          }}>
+                          {item.views_count}
+                        </Text>
+                      </View>
+
+                      {/* Likes */}
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                        }}>
+                        <FontAwesomeIcon
+                          icon={faHeart}
+                          size={16}
+                          color="#95a5a6"
+                          style={{
+                            marginRight: 5,
+                          }}
+                        />
+                        <Text
+                          style={{
+                            fontSize: 12,
+                            color: '#95a5a6',
+                          }}>
+                          {item.total_likes}
+                        </Text>
+                      </View>
+
+                      {/* Chapters */}
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                        }}>
+                        <FontAwesomeIcon
+                          icon={faList}
+                          size={16}
+                          color="#95a5a6"
+                          style={{
+                            marginRight: 5,
+                          }}
+                        />
+                        <Text
+                          style={{
+                            fontSize: 12,
+                            color: '#95a5a6',
+                          }}>
+                          {item.total_chapters}
+                        </Text>
+                      </View>
+                    </View>
+
+                    {/* Genre */}
+                    <View>
+                      <FlatList
+                        horizontal={true}
+                        data={item.genres}
+                        keyExtractor={item => item.id.toString()}
+                        renderItem={({item}) => (
+                          <View
+                            style={{
+                              backgroundColor: '#ecf0f1',
+                              borderRadius: 12,
+                              paddingHorizontal: 8,
+                              paddingVertical: 4,
+                              marginRight: 8,
+                            }}>
+                            <Text
+                              style={{
+                                fontSize: 12,
+                                color: '#7f8c8d',
+                              }}>
+                              {item.name}
+                            </Text>
+                          </View>
+                        )}
+                      />
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            )}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
